487d062e6b111f0fd17c2616ae8fe49bf8be2b4e
Abdullah Bin Faisal <abdullah@eecs.tufts.edu>
2020-03-02 19:15:45 -0500

    Add files via upload

---
+++ setup_ssh_keys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# create N keys
+for i in 0 1 2 3
+do
+	ssh-keygen -f k$i
+done
+
+#append them to authorized
+cat ~/.ssh/authorized_keys k0.pub k1.pub k2.pub k3.pub > authorized_keys
+
+#disseminate to all nodes
+for i in 0 1 2 3
+do
+	cat k$i | sudo ssh n$i "cat > /users/abdffsm/.ssh/key"
+	cat k$i.pub | sudo ssh n$i "cat > /users/abdffsm/.ssh/key.pub"
+	cat authorized_keys | sudo ssh n$i "cat > /users/abdffsm/.ssh/authorized_keys"
+done
